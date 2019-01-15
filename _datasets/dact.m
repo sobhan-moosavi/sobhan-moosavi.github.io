6599db064eabd5ec3305f95358501a3371c39c6c
Sobhan Moosavi <sobhan.mehr84@gmail.com>
2019-01-15 19:51:25 +0000

    Create dact.m

---
+++ _datasets/dact.m
@@ -0,0 +1,8 @@
+---
+title: "A Dataset of Annotated Car Trajectories (DACT)"
+collection: datastes
+permalink: /datasets/dact
+excerpt: DACT contains two subsets of annotated car trajectories data. The dataset contains 50 trajectories which cover about 13 hours of driving data. In DACT, we manually specified significant driving patterns by using an interactive framework. A significant driving pattern can be anything like a turn, speed-up, slow-down, etc. The annotation process consists of a crowd-sourcing task followed by comprehensive aggregation phases. The aggregation is done by two different strategies: Strict and Easy. For the first one, we used some strict constraints to aggregate crowd-sourcing results, while we used flexible constraints to generate the second subset of DACT.
+date: 2017-05-14
+---
+This paper is about a comprehensive framework to comapre web search engines on Persian web. 
